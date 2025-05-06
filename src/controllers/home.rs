d239d22ea9bff4a80327494f475bd05234f3beab
Andrey Maksimov <andrey@tenderowl.com>
2025-05-07 02:32:36 +0300

    Add home controller and view

--- src/controllers/home.rs
+++ src/controllers/home.rs
@@ -1,11 +1,12 @@
+use crate::views;
 use axum::debug_handler;
 use loco_rs::prelude::*;
 
 #[debug_handler]
-pub async fn index(State(_ctx): State<AppContext>) -> Result<Response> {
-    format::empty()
+pub async fn render_home(ViewEngine(v): ViewEngine<TeraView>) -> Result<impl IntoResponse> {
+    views::home::home(v)
 }
 
 pub fn routes() -> Routes {
-    Routes::new().add("/", get(index))
+    Routes::new().add("/", get(render_home))
 }
